790bb958bdfcea032d942dcaff73cca5b49e42c4
Santiago <santiago_figueroa_@hotmail.com>
2020-06-12 08:34:04 +0100

    Added CommanderContext class for SQL conectivity

--- Data/CommanderContext.cs
+++ Data/CommanderContext.cs
@@ -1,15 +1,15 @@
 using Commander.Models;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace Commander.Data
 {
 public class CommanderContext : DbContext
     {
 
-
-
-
-
+        public CommanderContext(DbContextOptions<CommanderContext> opt):base(opt)
+        {
+            
+        }
 
     }
 
