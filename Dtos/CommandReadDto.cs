093cdad42be24fa6be7ea482200fe2216e7c664c
Santiago <santiago_figueroa_@hotmail.com>
2020-07-08 12:30:26 +0100

    DTO class added to project.

---
+++ Dtos/CommandReadDto.cs
@@ -0,0 +1,12 @@
+namespace Commander.Dtos
+{
+    /* 
+    * Class to handle the DTO object. 
+    */
+    public class CommandReadDto 
+    {
+    public int Id {get;set; }
+    public string  Howto {get; set;}
+    public string  Line {get;set;}
+    }
+}
